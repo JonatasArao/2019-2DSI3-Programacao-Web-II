bd7564e46222f5264e17cf85f0e709d525bb7a24
Jonatas Arão de Carvalho Neves <jonatas.arao.de.carvalho.neves@hotmail.com>
2019-07-29 22:50:35 -0300

    Aula 01 - PHP Chegou

---
+++ Aula_01/index.php
@@ -0,0 +1,17 @@
+<?php
+
+echo('Hello, </br>');
+echo('World!!!<br><br>');
+
+$nome = 'Tony ';
+$sobrenome = "Stark";
+$nome2 = '$nome Stark<br><br>';
+
+echo $nome2;
+
+$a = "3 macacos";
+$b = "5 elefantes";
+
+echo $a + $b;
+
+//var_dump($b);
