a5a5cdc355e5a3979ed77eff0376cd2dca2266c4
Jonatas Arão de Carvalho Neves <jonatas.arao.de.carvalho.neves@hotmail.com>
2019-08-27 19:47:03 -0300

    Lista de Exercicios

---
+++ Aula_08/Exercicio_1/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Exercício 1</title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
+</head>
+
+<body>
+
+    <form action="" method="post">
+    
+    </form>
+
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+</body>
+
+</html>
